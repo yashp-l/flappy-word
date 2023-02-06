68c31b59adbb3ebaff804932ad396d76afb920ba
yashp-l <124635212+yashp-l@users.noreply.github.com>
2023-02-06 17:25:37 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,22 @@
+BACKGROUND_IMAGE = sprites.background.autumn
+YELLOW_BIRD = sprites.create(img("""
+        . . . . . . . . . . b 5 b . . . 
+            . . . . . . . . . b 5 b . . . . 
+            . . . . . . . . . b c . . . . . 
+            . . . . . . b b b b b b . . . . 
+            . . . . . b b 5 5 5 5 5 b . . . 
+            . . . . b b 5 d 1 f 5 5 d f . . 
+            . . . . b 5 5 1 f f 5 d 4 c . . 
+            . . . . b 5 5 d f b d d 4 4 . . 
+            b d d d b b d 5 5 5 4 4 4 4 4 b 
+            b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+            b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+            c d d c d 5 5 b 5 5 5 5 5 5 b . 
+            c b d d c c b 5 5 5 5 5 5 5 b . 
+            . c d d d d d d 5 5 5 5 5 d b . 
+            . . c b d d d d d 5 5 5 b b . . 
+            . . . c c c c c c c c b b . . .
+    """),
+    SpriteKind.player)
+YELLOW_BIRD.set_stay_in_screen(True)
+scene.set_background_image(BACKGROUND_IMAGE)
