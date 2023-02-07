b25d89abd0379b95978ede0ead37985fce83d6e7
yashp-l <124635212+yashp-l@users.noreply.github.com>
2023-02-06 21:39:18 -0500

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -2,35 +2,48 @@ namespace SpriteKind {
     export const Object = SpriteKind.create()
     export const Title = SpriteKind.create()
 }
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    YELLOW_BIRD.vy = -20
+controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
+    YELLOW_BIRD = sprites.create(img`
+        . . . . . . . . . . b 5 b . . . 
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . . . . b b 5 d 1 f 5 d 4 c . . 
+        . . . . b 5 5 1 f f d d 4 4 4 b 
+        . . . . b 5 5 d f b 4 4 4 4 b . 
+        . . . b d 5 5 5 5 4 4 4 4 b . . 
+        . . b d d 5 5 5 5 5 5 5 5 b . . 
+        . b d d d d 5 5 5 5 5 5 5 5 b . 
+        b d d d b b b 5 5 5 5 5 5 5 b . 
+        c d d b 5 5 d c 5 5 5 5 5 5 b . 
+        c b b d 5 d c d 5 5 5 5 5 5 b . 
+        . b 5 5 b c d d 5 5 5 5 5 d b . 
+        b b c c c d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
+        `, SpriteKind.Player)
+    cont1 = 1
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    YELLOW_BIRD.vy = -100
+    YELLOW_BIRD.vx += -20
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    YELLOW_BIRD.vy = -100
+    YELLOW_BIRD.vx += 20
 })
 let YELLOW_BIRD: Sprite = null
-let TITLE_X = 0
+let cont1 = 0
 let TITLE = sprites.create(assets.image`title`, SpriteKind.Title)
-TITLE.setVelocity(50, 50)
-TITLE.setPosition(TITLE_X, 40)
-let BACKGROUND = 0
-YELLOW_BIRD = sprites.create(img`
-    . . . . . . . . . . b 5 b . . . 
-    . . . . . . . . . b 5 b . . . . 
-    . . . . . . b b b b b b . . . . 
-    . . . . . b b 5 5 5 5 5 b . . . 
-    . . . . b b 5 d 1 f 5 d 4 c . . 
-    . . . . b 5 5 1 f f d d 4 4 4 b 
-    . . . . b 5 5 d f b 4 4 4 4 b . 
-    . . . b d 5 5 5 5 4 4 4 4 b . . 
-    . . b d d 5 5 5 5 5 5 5 5 b . . 
-    . b d d d d 5 5 5 5 5 5 5 5 b . 
-    b d d d b b b 5 5 5 5 5 5 5 b . 
-    c d d b 5 5 d c 5 5 5 5 5 5 b . 
-    c b b d 5 d c d 5 5 5 5 5 5 b . 
-    . b 5 5 b c d d 5 5 5 5 5 d b . 
-    b b c c c d d d d 5 5 5 b b . . 
-    . . . c c c c c c c c b b . . . 
-    `, SpriteKind.Player)
-YELLOW_BIRD.setPosition(9, 40)
-scene.cameraFollowSprite(YELLOW_BIRD)
+let BACKGROUND = sprites.background.cityscape
+cont1 = 0
+scene.setBackgroundImage(BACKGROUND)
+TITLE.setPosition(80, 40)
+YELLOW_BIRD.setStayInScreen(true)
+scaling.scaleByPercent(TITLE, 100, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+pause(5000)
+if (cont1 == 1) {
+    YELLOW_BIRD.setPosition(9, 40)
+}
 forever(function () {
     YELLOW_BIRD.setImage(img`
         . . . . . . . . . . . . . . . . 
@@ -70,6 +83,8 @@ forever(function () {
         . . . c c c c c c c c b b . . . 
         `)
     pause(200)
-    YELLOW_BIRD.vy += 10
-    YELLOW_BIRD.vx += 5
+})
+forever(function () {
+    YELLOW_BIRD.vy += 30
+    pause(50)
 })
