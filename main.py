b642a02adfe402349825a5863df9b222fd90a99f
yashp-l <124635212+yashp-l@users.noreply.github.com>
2023-02-06 19:46:17 -0500

    Update pxt.json, main.blocks, main.ts, main.py, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.py
+++ main.py
@@ -1,22 +1,69 @@
-BACKGROUND_IMAGE = sprites.background.autumn
-YELLOW_BIRD = sprites.create(img("""
+def on_a_pressed():
+    mySprite.vy = -20
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
+mySprite: Sprite = None
+mySprite = sprites.create(img("""
         . . . . . . . . . . b 5 b . . . 
             . . . . . . . . . b 5 b . . . . 
-            . . . . . . . . . b c . . . . . 
             . . . . . . b b b b b b . . . . 
             . . . . . b b 5 5 5 5 5 b . . . 
-            . . . . b b 5 d 1 f 5 5 d f . . 
-            . . . . b 5 5 1 f f 5 d 4 c . . 
-            . . . . b 5 5 d f b d d 4 4 . . 
-            b d d d b b d 5 5 5 4 4 4 4 4 b 
-            b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
-            b d c 5 5 5 5 d 5 5 5 5 5 b . . 
-            c d d c d 5 5 b 5 5 5 5 5 5 b . 
-            c b d d c c b 5 5 5 5 5 5 5 b . 
-            . c d d d d d d 5 5 5 5 5 d b . 
-            . . c b d d d d d 5 5 5 b b . . 
+            . . . . b b 5 d 1 f 5 d 4 c . . 
+            . . . . b 5 5 1 f f d d 4 4 4 b 
+            . . . . b 5 5 d f b 4 4 4 4 b . 
+            . . . b d 5 5 5 5 4 4 4 4 b . . 
+            . . b d d 5 5 5 5 5 5 5 5 b . . 
+            . b d d d d 5 5 5 5 5 5 5 5 b . 
+            b d d d b b b 5 5 5 5 5 5 5 b . 
+            c d d b 5 5 d c 5 5 5 5 5 5 b . 
+            c b b d 5 d c d 5 5 5 5 5 5 b . 
+            . b 5 5 b c d d 5 5 5 5 5 d b . 
+            b b c c c d d d d 5 5 5 b b . . 
             . . . c c c c c c c c b b . . .
     """),
     SpriteKind.player)
-YELLOW_BIRD.set_stay_in_screen(True)
-scene.set_background_image(BACKGROUND_IMAGE)
+mySprite.set_position(9, 40)
+scene.camera_follow_sprite(mySprite)
+
+def on_forever():
+    mySprite.set_image(img("""
+        . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . b 5 b . . . 
+                . . . . . . . . . b 5 b . . . . 
+                . . . . . . b b b b b b . . . . 
+                . . . . . b b 5 5 5 5 5 b . . . 
+                . b b b b b 5 5 5 5 5 5 5 b . . 
+                . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+                . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+                . . b d 5 5 b 1 f f 5 4 4 c . . 
+                b b d b 5 5 5 d f b 4 4 4 4 4 b 
+                b d d c d 5 5 b 5 4 4 4 4 4 b . 
+                c d d d c c b 5 5 5 5 5 5 5 b . 
+                c b d d d d d 5 5 5 5 5 5 5 b . 
+                . c d d d d d d 5 5 5 5 5 d b . 
+                . . c b d d d d d 5 5 5 b b . . 
+                . . . c c c c c c c c b b . . .
+    """))
+    pause(200)
+    mySprite.set_image(img("""
+        . . . . . . . . . . b 5 b . . . 
+                . . . . . . . . . b 5 b . . . . 
+                . . . . . . b b b b b b . . . . 
+                . . . . . b b 5 5 5 5 5 b . . . 
+                . . . . b b 5 d 1 f 5 d 4 c . . 
+                . . . . b 5 5 1 f f d d 4 4 4 b 
+                . . . . b 5 5 d f b 4 4 4 4 b . 
+                . . . b d 5 5 5 5 4 4 4 4 b . . 
+                . . b d d 5 5 5 5 5 5 5 5 b . . 
+                . b d d d d 5 5 5 5 5 5 5 5 b . 
+                b d d d b b b 5 5 5 5 5 5 5 b . 
+                c d d b 5 5 d c 5 5 5 5 5 5 b . 
+                c b b d 5 d c d 5 5 5 5 5 5 b . 
+                . b 5 5 b c d d 5 5 5 5 5 d b . 
+                b b c c c d d d d 5 5 5 b b . . 
+                . . . c c c c c c c c b b . . .
+    """))
+    pause(200)
+    mySprite.vy += 10
+    mySprite.vx += 5
+forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,21 +1,75 @@
-let BACKGROUND_IMAGE = sprites.background.autumn
-let YELLOW_BIRD = sprites.create(img`
+namespace SpriteKind {
+    export const Object = SpriteKind.create()
+    export const Title = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    YELLOW_BIRD.vy = -20
+})
+let YELLOW_BIRD: Sprite = null
+let TITLE_X = 0
+let TITLE = sprites.create(assets.image`title`, SpriteKind.Title)
+TITLE.setVelocity(50, 50)
+TITLE.setPosition(TITLE_X, 40)
+let BACKGROUND = 0
+YELLOW_BIRD = sprites.create(img`
     . . . . . . . . . . b 5 b . . . 
     . . . . . . . . . b 5 b . . . . 
-    . . . . . . . . . b c . . . . . 
     . . . . . . b b b b b b . . . . 
     . . . . . b b 5 5 5 5 5 b . . . 
-    . . . . b b 5 d 1 f 5 5 d f . . 
-    . . . . b 5 5 1 f f 5 d 4 c . . 
-    . . . . b 5 5 d f b d d 4 4 . . 
-    b d d d b b d 5 5 5 4 4 4 4 4 b 
-    b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
-    b d c 5 5 5 5 d 5 5 5 5 5 b . . 
-    c d d c d 5 5 b 5 5 5 5 5 5 b . 
-    c b d d c c b 5 5 5 5 5 5 5 b . 
-    . c d d d d d d 5 5 5 5 5 d b . 
-    . . c b d d d d d 5 5 5 b b . . 
+    . . . . b b 5 d 1 f 5 d 4 c . . 
+    . . . . b 5 5 1 f f d d 4 4 4 b 
+    . . . . b 5 5 d f b 4 4 4 4 b . 
+    . . . b d 5 5 5 5 4 4 4 4 b . . 
+    . . b d d 5 5 5 5 5 5 5 5 b . . 
+    . b d d d d 5 5 5 5 5 5 5 5 b . 
+    b d d d b b b 5 5 5 5 5 5 5 b . 
+    c d d b 5 5 d c 5 5 5 5 5 5 b . 
+    c b b d 5 d c d 5 5 5 5 5 5 b . 
+    . b 5 5 b c d d 5 5 5 5 5 d b . 
+    b b c c c d d d d 5 5 5 b b . . 
     . . . c c c c c c c c b b . . . 
     `, SpriteKind.Player)
-YELLOW_BIRD.setStayInScreen(true)
-scene.setBackgroundImage(BACKGROUND_IMAGE)
+YELLOW_BIRD.setPosition(9, 40)
+scene.cameraFollowSprite(YELLOW_BIRD)
+forever(function () {
+    YELLOW_BIRD.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . b 5 b . . . 
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . b b b b b 5 5 5 5 5 5 5 b . . 
+        . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+        . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+        . . b d 5 5 b 1 f f 5 4 4 c . . 
+        b b d b 5 5 5 d f b 4 4 4 4 4 b 
+        b d d c d 5 5 b 5 4 4 4 4 4 b . 
+        c d d d c c b 5 5 5 5 5 5 5 b . 
+        c b d d d d d 5 5 5 5 5 5 5 b . 
+        . c d d d d d d 5 5 5 5 5 d b . 
+        . . c b d d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
+        `)
+    pause(200)
+    YELLOW_BIRD.setImage(img`
+        . . . . . . . . . . b 5 b . . . 
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . . . . b b 5 d 1 f 5 d 4 c . . 
+        . . . . b 5 5 1 f f d d 4 4 4 b 
+        . . . . b 5 5 d f b 4 4 4 4 b . 
+        . . . b d 5 5 5 5 4 4 4 4 b . . 
+        . . b d d 5 5 5 5 5 5 5 5 b . . 
+        . b d d d d 5 5 5 5 5 5 5 5 b . 
+        b d d d b b b 5 5 5 5 5 5 5 b . 
+        c d d b 5 5 d c 5 5 5 5 5 5 b . 
+        c b b d 5 d c d 5 5 5 5 5 5 b . 
+        . b 5 5 b c d d 5 5 5 5 5 d b . 
+        b b c c c d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
+        `)
+    pause(200)
+    YELLOW_BIRD.vy += 10
+    YELLOW_BIRD.vx += 5
+})
